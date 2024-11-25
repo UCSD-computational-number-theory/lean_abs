8a9d213a042a0cc9eea4b5c136c3760db3b0aa1a
pink10000 <kyletrinh1223@gmail.com>
2024-11-25 10:36:30 -0800

    try minimal_prime_IsMin

--- LeanCommAlg/Basic.lean
+++ LeanCommAlg/Basic.lean
@@ -1,11 +1,10 @@
 import Mathlib.AlgebraicGeometry.PrimeSpectrum.Maximal
+import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.RingTheory.Ideal.MinimalPrime
 import Mathlib.RingTheory.Ideal.Operations
-import Mathlib.RingTheory.Ideal.Basic
 import Mathlib.RingTheory.PrincipalIdealDomain
-import Mathlib.Order.WithBot
 import Mathlib.Order.Height
-import Mathlib.RingTheory.Ideal.Basic
+import Mathlib.Order.WithBot
 import Mathlib.Order.KrullDimension
 import Mathlib.LinearAlgebra.Span.Defs
 --import Mathlib.Data.Real.ENNReal
@@ -17,13 +16,35 @@ variable (I : Ideal R)
 #check Ideal.IsPrime
 
 noncomputable def height [h : I.IsPrime] : WithBot ℕ∞ :=
-  Order.height {J : PrimeSpectrum R | J.asIdeal ≤ I}
+  Order.height {J : PrimeSpectrum R | J.asIdeal < I}
+
+-- noncomputable def height [h : I.IsPrime] : WithBot ℕ∞ :=
+--   Order.height (fun _ : PrimeSpectrum R => I)
 
 #check height
 
---noncomputable def height [h : IsPrime I] : WithBot ℕ∞ :=
-  --Order.height (PrimeSpectrum R) (I : PrimeSpectrum R)
+-- Minimal prime proof
+theorem minimal_prime_IsMin (I : Ideal R) (P : Ideal R)
+    (Pmin : P ∈ Ideal.minimalPrimes I) :
+    IsMin P := by
+  rw [Ideal.minimalPrimes] at Pmin
+  rcases Pmin with ⟨hPrime, hMinimal⟩
+  intros Q hQ
+  have QPrime : Q.IsPrime := by
+    sorry
+  have IleQ : I ≤ Q := by
+    sorry
+  specialize hMinimal ?_
+  apply Q; simp [IleQ, QPrime];
+  exact hMinimal (by assumption)
 
+
+lemma height_eq_zero_if_minimal [h : I.IsPrime] :
+    I ∈ minimalPrimes R → height I = 0 := by
+  intro hmin
+  unfold height
+  simp
+  sorry
 
 theorem height_1_of_principal_of_prime [h : I.IsPrime] [h' : I.IsPrincipal] : height I ≤ 1 := by
   rw [height, Order.height]
