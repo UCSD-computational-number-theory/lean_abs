5e145101ce23a6bf02070fa0db7dac7f1e2a07c5
arnavdandu <adandu@ucsd.edu>
2024-11-29 15:29:56 -0800

    added lemma singleton_of_minimal_prime

--- LeanCommAlg/Basic.lean
+++ LeanCommAlg/Basic.lean
@@ -16,17 +16,17 @@ variable (I : Ideal R)
 #check Ideal.IsPrime
 
 noncomputable def height [h : I.IsPrime] : WithBot ℕ∞ :=
-  Order.height {J : PrimeSpectrum R | J.asIdeal < I}
+  Order.height {J : PrimeSpectrum R | J.asIdeal ≤ I}
 
 -- noncomputable def height [h : I.IsPrime] : WithBot ℕ∞ :=
 --   Order.height (fun _ : PrimeSpectrum R => I)
 
 #check height
 
+
 -- Minimal prime proof
-theorem minimal_prime_IsMin (I : Ideal R) (P : Ideal R)
-    (Pmin : P ∈ Ideal.minimalPrimes I) :
-    IsMin P := by
+theorem minimal_prime_IsMin
+    (I : Ideal R) (P : Ideal R) (Pmin : P ∈ Ideal.minimalPrimes I) : IsMin P := by
   rw [Ideal.minimalPrimes] at Pmin
   rcases Pmin with ⟨hPrime, hMinimal⟩
   intros Q hQ
@@ -38,12 +38,40 @@ theorem minimal_prime_IsMin (I : Ideal R) (P : Ideal R)
   apply Q; simp [IleQ, QPrime];
   exact hMinimal (by assumption)
 
+lemma singleton_of_minimal_prime [h : I.IsPrime] :
+    I ∈ minimalPrimes R → {J : PrimeSpectrum R | J.asIdeal ≤ I} = {⟨I, h⟩} := by
+  intro hmin
+  rcases hmin with ⟨hl, hr⟩
+  dsimp at hr
+  ext J
+  simp [PrimeSpectrum.ext_iff]
+  constructor
+  . intro hJ
+    simp at hJ
+    specialize hr ⟨J.2, bot_le⟩
+    have : I ≤ J.asIdeal := by
+      apply hr hJ
+    apply le_antisymm hJ this
+  . intro hJ
+    apply le_iff_lt_or_eq.mpr
+    right
+    apply hJ
 
-lemma height_eq_zero_if_minimal [h : I.IsPrime] :
+lemma height_zero_of_minimal_prime [h : I.IsPrime] :
     I ∈ minimalPrimes R → height I = 0 := by
   intro hmin
-  unfold height
+  rcases hmin with ⟨hl, hr⟩
+  dsimp at hr
+  have : {J : PrimeSpectrum R | J.asIdeal ≤ I} = {⟨I, h⟩} := by
+    apply singleton_of_minimal_prime
+  rw [height, Order.height]
+  rw [this]
   simp
+  intros lt hy
+  rw [RelSeries.length_eq_zero]
+  intro x
+  simp
+  intros xlt y ylt
   sorry
 
 theorem height_1_of_principal_of_prime [h : I.IsPrime] [h' : I.IsPrincipal] : height I ≤ 1 := by
