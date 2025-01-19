a92396fb1b8ba6d57a44d4960ebcd20f3e265e09
pink10000 <kyletrinh1223@gmail.com>
2025-01-19 09:28:59 -0800

    radical intersection

--- LeanCommAlg/Basic.lean
+++ LeanCommAlg/Basic.lean
@@ -51,10 +51,10 @@ noncomputable def height (p : Ideal R) [hp : p.IsPrime] : WithBot ℕ∞ :=
   rw [height, Order.height] at height_neq_0
   simp [height] at *
   obtain ⟨ltseries, ⟨rel_last, len_neq_0⟩⟩ := height_neq_0
-  have head_le_last : RelSeries.head ltseries < RelSeries.last ltseries := by
+  have head_lt_last : RelSeries.head ltseries < RelSeries.last ltseries := by
     apply RelSeries.rel_of_lt; contrapose! len_neq_0; simp_all
   have head_lt_I : (RelSeries.head ltseries).asIdeal < I := by
-    exact gt_of_ge_of_gt rel_last head_le_last
+    exact gt_of_ge_of_gt rel_last head_lt_last
   have head_le_I : (RelSeries.head ltseries).asIdeal ≤ I := by
     apply le_of_lt head_lt_I
   specialize y_minimal ?_
@@ -68,8 +68,20 @@ I think this should be true? If you consider the chain of ideals, then `J` must 
 -/
 lemma height_le_prime_of_minimal_prime (J : Ideal R) [hJ : J.IsPrime] [hI : I.IsPrime] :
     J ∈ I.minimalPrimes → height I ≤ height J  := by
-  intro hmin
+  intro Imin; rcases Imin with ⟨I_le_J, y_minimal⟩; simp_all
+  by_contra hJ_lt_hI; simp [not_le_of_lt] at hJ_lt_hI
+
+  simp_all [height, Order.height]
   sorry
+
+theorem height_eq_iff_eq (I J : Ideal R) [hJ : J.IsPrime] [hI : I.IsPrime] :
+    I = J ↔ height I = height J := by
+  constructor
+  . case mp => intro I_eq_J; subst I_eq_J; rfl
+  . case mpr =>
+    intro h_eq
+    simp [height, Order.height] at h_eq
+    sorry
 
 theorem height_1_of_principal_of_prime [h : I.IsPrime] [h' : I.IsPrincipal] : height I ≤ 1 := by
   rw [height, Order.height]
@@ -77,3 +89,15 @@ theorem height_1_of_principal_of_prime [h : I.IsPrime] [h' : I.IsPrincipal] : he
   intro ltseries relseries
   sorry
   -- Associated primes, Krull's principal ideal theorem,
+
+-- rad(I) is the intersection of all minimal primes containing I
+theorem radical_intersection_minimal_primes (I : Ideal R) :
+    I.radical = ⨅ (P : Ideal R) (hP : P ∈ I.minimalPrimes), P := by
+  ext x; constructor
+  case h.mp =>
+    intro x_in_rad
+    sorry
+  case h.mpr =>
+    intro x_in_inter
+    simp at x_in_inter
+    sorry
