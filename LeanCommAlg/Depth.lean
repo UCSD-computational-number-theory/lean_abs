4e8868d57dd35613fbc91542ac7cfdd9c31c2033
Arnav Dandu <arnavdandu@gmail.com>
2025-02-11 18:09:57 -0800

    finished depth definition

--- LeanCommAlg/Depth.lean
+++ LeanCommAlg/Depth.lean
@@ -11,9 +11,8 @@ variable [CommRing R] [AddCommGroup M] [Module R M]
 
 variable (rs : List R)
 
-noncomputable def depth (I : Ideal R) [Module R M] : ℕ :=
-  ⨆(rs : List R), ⨆(_ : Sequence.IsRegular M rs), rs.length
-
-
 def list_I_to_R [CommRing R] (I : Ideal R) (L : List I) : List R :=
   (L : List R)
+
+noncomputable def depth (I : Ideal R) [Module R M] : ℕ :=
+  ⨆(rs : List I), ⨆(_ : Sequence.IsRegular M (list_I_to_R I rs)), rs.length
